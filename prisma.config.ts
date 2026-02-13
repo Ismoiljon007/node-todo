8fb327f8c64750c819176f7275de50459a541cb2
ismoiljon007 <ibahtiyorov007@gmail.com>
2026-02-13 17:28:20 +0500

    refactor: update build script to include prisma generation

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,8 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
-const databaseUrl = process.env.DATABASE_URL;
-
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL topilmadi. .env faylni tekshiring.");
-}
+const databaseUrl =
+  process.env.DATABASE_URL
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
